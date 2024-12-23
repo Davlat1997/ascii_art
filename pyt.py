9b354efc3efb2eb54eebc570115826369040f952
Davlat1997 <aslamovavazira818@gmail.com>
2024-12-23 15:50:23 +0500

    donee

---
+++ pyt.py
@@ -0,0 +1,30 @@
+print(f"""   
+            |\_/|
+            | @ @      Woof!
+            |   <>                        -
+            |  _/\ ---------_______    (( | ))         
+            |                      `---   |
+            |                             '
+     _______|__           ______|   |_____.
+    / _  / _______/ _____/  ________|
+      """
+)
+
+print(f"   ooooo")
+print(f"  o     o")
+print(f" o       o")
+print(f"  o     o")
+print(f"   ooooo")
+print(f"     |")
+print(f"   --|--")
+print(f"     |")
+print(f"    / \\")
+print(f"   /   \\")
+print(f"  /     \\")
+print(f" /       \\")
+print(f"  |#####|")
+print(f"   #####")
+print(f"     #")
+print(f"     |")
+print(f"    / \\")
+print(f"   /   \\")
